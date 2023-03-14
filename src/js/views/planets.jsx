6163f197692fa4e049325264c25e1951b94f3e1e
NurFer13 <nurfer1307@gmail.com>
2023-03-14 21:53:31 +0000

    card

--- src/js/views/planets.jsx
+++ src/js/views/planets.jsx
@@ -1,9 +1,4 @@
-// crear comp de react q va renderizar las crtas
-//fetch a la ruta d la info q necesita planets
-//la info la meto en usestate q va a ser un array
-//mapeo array y el objeto m devuelve nombre,id,url(le hago un fetch dentro del mapeo)
-// lo vuelvo a almacenar en una array
-//al mapear la segunda array obtengo la info d los props xa ls cards
+
 import React from "react";
 
 const Planets = () =>{
