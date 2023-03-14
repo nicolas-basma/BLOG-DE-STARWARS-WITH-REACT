646a7d94bfc1c7cfaa97a1600d15d061ea88256f
Replit user <>
2023-03-14 20:51:02 +0000

    add routes

---
+++ src/js/routes/vehicles.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Vehicles = () => {
+	return (
+		fetch("https://www.swapi.tech/api/vehicles/4"
+			.then(res => res.json()
+				.then(data => console.log(data)
+					.catch(err => console.error(err)
+					)
+}
