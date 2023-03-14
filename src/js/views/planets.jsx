b8468474f0dc085b9d159f7b8bf1be39e046e6af
NurFer13 <nurfer1307@gmail.com>
2023-03-14 15:10:36 +0000

    planets

--- src/js/views/planets.jsx
+++ src/js/views/planets.jsx
@@ -7,9 +7,9 @@
 import React from "react";
 
 const Planets = () =>{
-    
+    const [planets,setplanets]= useState([]);
     const getPlanets=() => {
-        fetch("https://www.swapi.tech/api/planets")
+         fetch("https://www.swapi.tech/api/planets")
         .then(res => res.json())
         .then(data => console.log(data))
         .catch(err => console.error(err))
