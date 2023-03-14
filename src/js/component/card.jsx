26212e9e7b8553d4f7c59034ff9bb925804bfdab
NurFer13 <nurfer1307@gmail.com>
2023-03-14 12:14:30 +0000

    card bootstrap

---
+++ src/js/component/card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Card = () => {
+    return (
+    <div className="card" style="width: 18rem;">
+            <img src="..." className="card-img-top" alt="..."/>
+        <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Details</p>
+            <a href="#" className="btn btn-primary">Learn more!</a>
+        </div>
+    </div>
+     );
+}
