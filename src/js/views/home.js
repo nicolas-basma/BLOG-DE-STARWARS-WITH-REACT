326fd47b8eaefe0d785f32fccdf0fe2ae16e2861
NicoB <nicobasma_@hotmail.com>
2023-03-27 22:59:33 +0000

    solucionado el key

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -15,19 +15,19 @@ export const Home = () => {
 		<h1>Characters</h1>
       <div className="characterContainer">
 		{ listPeople
-		? listPeople.map((character)=> <CharactersCard key={character.uid} name={character.result.properties.name}  id={character.result.uid} eyeColor={character.result.properties.eye_color} gender={character.result.properties.gender}/>)
+		? listPeople.map((character)=> <CharactersCard key={character.result.uid} name={character.result.properties.name}  id={character.result.uid} eyeColor={character.result.properties.eye_color} gender={character.result.properties.gender}/>)
 		: null}
 			</div>
 		<h1>Vehicles</h1>
 			<div className="vehiclesContainer">
 				{ listVehicles
-				? listVehicles.map((vehicle) => <VehicleCard  key={vehicle.uid} name={vehicle.result.properties.name} id={vehicle.result.uid} cargoCapacity={vehicle.result.properties.cargo_capacity} passengers={vehicle.result.properties.passengers}  vehicleClass={vehicle.result.properties.vehicle_class} model={vehicle.result.properties.model} />)
+				? listVehicles.map((vehicle) => <VehicleCard  key={vehicle.result.uid} name={vehicle.result.properties.name} id={vehicle.result.uid} cargoCapacity={vehicle.result.properties.cargo_capacity} passengers={vehicle.result.properties.passengers}  vehicleClass={vehicle.result.properties.vehicle_class} model={vehicle.result.properties.model} />)
 			:null }
 			</div>
 		<h1>Planets</h1>
 			<div className="planetsContainer">
 				{ listPlanets
-				? listPlanets.map((planet) => <PlanetsCard key={planet.uid} name={planet.result.properties.name} id={planet.result.uid} terrain={planet.result.properties.terrain} population={planet.result.properties.population}/>)
+				? listPlanets.map((planet) => <PlanetsCard key={planet.result.uid} name={planet.result.properties.name} id={planet.result.uid} terrain={planet.result.properties.terrain} population={planet.result.properties.population}/>)
 			:null}
 			</div>
 	</div>
